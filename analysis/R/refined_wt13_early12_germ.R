a5474d5d1feb6f06832083112deb71481a55f0cb
dpeng28 <dpeng5@jh.edu>
2023-06-06 14:37:52 -0400

    cleaned and reran refined_wt13_early12_germ.R

--- analysis/R/refined_wt13_early12_germ.R
+++ analysis/R/refined_wt13_early12_germ.R
@@ -1,19 +1,16 @@
-library(monocle3)
+# analysis of germ cells 
 set.seed(123)
 TARGET_dir = file.path("results", ANALYSIS_VERSION, "refined_wt_late_early_germ")
 dir.create(TARGET_dir)
 
-wt_late_object = readRDS(file.path("results", ANALYSIS_VERSION, "manual_annotation_wt13/manual_celltype_object3.rds"))
-
-##########################
-# TODO the below will be changed as time goes on 
-wt_early_object = readRDS(file.path("results", ANALYSIS_VERSION, "early_wt12_integrated/BDGP_automated_annotation_object.rds"))
-sub_wt_early_object = subset(wt_early_object, subset = Integrated_tentativeCellType == 'germ cell')
-sub_wt_early_object$experimental_condition = 'early'
-########################
-
-sub_wt_late_object = subset(wt_late_object, subset = manual_celltypes == "Germ Cell")
+##### load in required data #####
+wt_late_object = readRDS(file.path("results", ANALYSIS_VERSION, "manual_annotation_wt13/manual_celltype_object4.rds"))
+sub_wt_late_object = subset(wt_late_object, subset = manual_celltypes == "Germ Cells")
 sub_wt_late_object$experimental_condition = 'late'
+
+wt_early_object = readRDS(file.path("results", ANALYSIS_VERSION, "manual_annotation_early_wt12/manual_celltype_object1.rds"))
+sub_wt_early_object = subset(wt_early_object, subset = manual_celltypes == 'Germ Cells')
+sub_wt_early_object$experimental_condition = 'early'
 
 combined_ct_object = merge(sub_wt_early_object, sub_wt_late_object)
 object = Seurat::CreateSeuratObject(combined_ct_object@assays$RNA@counts, project = 'Germ Cell')
@@ -24,7 +21,7 @@ object = Seurat::NormalizeData(object)
 cellCycleMarkers = read.csv("accessory_data/cellCycleMarkers.csv", skip = 1, header = T)
 object %<>% CellCycleScoring(s.features = cellCycleMarkers$S.phase.markers., g2m.features = cellCycleMarkers$G2.M.phase.markers.)
 
-# start monocle3 
+##### convert seurat to monocle3 #####
 expression_matrix = object@assays$RNA@counts
 cell_metadata = object@meta.data
 gene_annotation = object@assays$RNA@meta.features
@@ -43,10 +40,7 @@ cds <- cluster_cells(cds, resolution = 5e-3)
 plot_cells(cds, color_cells_by = "cluster", label_cell_groups = FALSE, cell_size = 1)
 ggsave(file.path(TARGET_dir, 'monocle3_no_batch_corrected_cluster.png'), width = 8, height = 6)
 
-marker_test_res <- monocle3::top_markers(cds, group_cells_by="cluster", 
-                                         reference_cells=1000, cores=8)
 
-write.csv(marker_test_res, file = file.path(TARGET_dir, "top_markers_monocle.csv"))
 rank_sum_results = presto::wilcoxauc(normalized_counts(cds), cds@clusters$UMAP$clusters)
 write.csv(rank_sum_results, file = file.path(TARGET_dir, "rank_sum_test.csv"))
 
@@ -66,60 +60,7 @@ plot_cells(cds,
 ggsave(file.path(TARGET_dir, 'monocle3_no_batch_corrected_pt.png'), width = 8, height = 6)
 saveRDS(cds, file = file.path(TARGET_dir, "monocle3_no_batch_correct_object.rds"))
 
-########################################
-# to test out the main trajectory 
-main_cds = cds[, monocle3::clusters(cds) != 3 | monocle3::clusters(cds) != 1]
-expression_matrix = monocle3::normalized_counts(main_cds)
-expRaw = expression_matrix[apply(expression_matrix, MARGIN = 1, FUN = function(x){return(sum(x > 0) > 10) }), ]
-pt<-as.data.frame(pseudotime(main_cds))
-colnames(pt)<-"pseudotime"
-cw<-as.matrix(rep(1,nrow(pt)))
-rownames(cw)<-rownames(pt)
-ts<-tradeSeq::fitGAM(as.matrix(expRaw),pseudotime=as.matrix(pt),cellWeights=cw, parallel = TRUE)
-saveRDS(ts, file = file.path(TARGET_dir, "main_tradeseq_fitgam_results.rds"))
-ATres<-tradeSeq::associationTest(ts)
-saveRDS(ATres, file = file.path(TARGET_dir, 'main_raw_associationTest.rds'))
-
-########################################
-# to test out the main trajectory 
-sub_cds = cds[, monocle3::clusters(cds) == 3 | monocle3::clusters(cds) == 1]
-expression_matrix = monocle3::normalized_counts(sub_cds)
-expRaw = expression_matrix[apply(expression_matrix, MARGIN = 1, FUN = function(x){return(sum(x > 0) > 10) }), ]
-pt<-as.data.frame(pseudotime(sub_cds))
-colnames(pt)<-"pseudotime"
-cw<-as.matrix(rep(1,nrow(pt)))
-rownames(cw)<-rownames(pt)
-ts<-tradeSeq::fitGAM(as.matrix(expRaw),pseudotime=as.matrix(pt),cellWeights=cw, parallel = TRUE)
-saveRDS(ts, file = file.path(TARGET_dir, "sub_tradeseq_fitgam_results.rds"))
-ATres<-tradeSeq::associationTest(ts)
-saveRDS(ATres, file = file.path(TARGET_dir, 'sub_raw_associationTest.rds'))
-
-
-########################################
-# let's solve the identity the clusters
-library(enrichR)
-enrichR::setEnrichrSite("FlyEnrichr")
-dir.create(file.path(TARGET_dir, 'cluster_process'))
-rank_sum_results = read.csv(file.path(TARGET_dir, "rank_sum_test.csv"), row.names = 1)
-
-withr::with_dir(file.path(TARGET_dir, 'cluster_process'), {
-  for(cluster in unique(rank_sum_results$group)) {
-    sub_marker_test_res = rank_sum_results[rank_sum_results$group == cluster, ]
-    sub_marker_test_res = sub_marker_test_res[sub_marker_test_res$logFC > 0.1, ]
-    sub_marker_test_res = sub_marker_test_res[sub_marker_test_res$padj < 0.05, ]
-    enrichment_results = enrichR::enrichr(
-      genes = sub_marker_test_res$feature, 
-      databases = c(
-        "GO_Biological_Process_2018", 
-        "GO_Molecular_Function_2018"
-      )
-    )
-    biological_analysis = enrichment_results$GO_Biological_Process_2018
-    write.csv(biological_analysis, file = paste0(cluster, "_biological_process.csv"))
-  }
-})
-
-library(fgsea) 
+##### compute GSEA #####
 dir.create(file.path(TARGET_dir, 'cluster_process_GSEA'))
 
 pathway_list = readRDS('accessory_data/GO_Biological_Processes_2018/GO_Biological_Process.rds')
@@ -141,32 +82,9 @@ withr::with_dir(file.path(TARGET_dir, 'cluster_process_GSEA'), {
     fgseaRes = as.data.frame(fgseaRes)
     fgseaRes$padj = as.numeric(fgseaRes$padj)
     fgseaRes = fgseaRes[!is.na(fgseaRes$padj), ]
-    #fgseaRes = fgseaRes[fgseaRes$pval < 0.05, ]
     fgseaRes$NES = as.numeric(fgseaRes$NES)
-    #fgseaRes = fgseaRes[fgseaRes$NES > 0, ]
     write.csv(fgseaRes, file = file.path(paste0(ct, '_gsea_results.csv')))
   }
 })
 
-##################
-# check y chromosome genes 
-cds = readRDS(file.path(TARGET_dir, "monocle3_no_batch_correct_object.rds"))
-genes_chrom = read.csv("../quantification/reference_genome_info/dmel-all-r6.33.gtf", header = FALSE, sep = '\t')
-genes_chrom = genes_chrom[genes_chrom$V1 == 'Y', ]
-genes_symbol = stringr::str_split(genes_chrom$V9, ";", simplify = TRUE)
-genes_symbol = as.data.frame(genes_symbol)
-genes_symbol = stringr::str_remove_all(genes_symbol$V2, " gene_symbol ")
-y_genes = unique(genes_symbol)
-
-dir.create(file.path(TARGET_dir, 'Y_chromosome_genes'))
-
-withr::with_dir(file.path(TARGET_dir, 'Y_chromosome_genes'), {
-  for(y_gene in y_genes) {
-    plot_cells(cds, genes = y_gene, cell_size = 1)
-    ggsave(paste0(y_gene, "_exp.png"))
-  }
-})
-
-##### Let's look at some of the things for #####
-cds = readRDS(file.path(TARGET_dir, "monocle3_no_batch_correct_object.rds"))
 
