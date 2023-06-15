7a61d7745514ae64a0e4efebba230885e62d5a12
dpeng28 <dpeng5@jh.edu>
2023-06-15 11:24:57 -0400

    modified plotting for tracheal tip cell marker genes

--- analysis/R/plot_refined_wt13_early12_trachea.R
+++ analysis/R/plot_refined_wt13_early12_trachea.R
@@ -120,7 +120,7 @@ p = ggplot(UMAP_coord, aes(x=reorder(cell_type, -btl), y=btl, fill = cell_type))
   theme_minimal() +
   scale_fill_brewer(palette = 'Set2', breaks=c('Early Tracheal Cells', 'Interm. Tracheal Cells', 'Late Tracheal Cells', 'Tracheal Tip Cells')) + 
   ylab("btl Normalized Expression") + 
-  xlab("cell type") + 
+  xlab("Cell Types") + 
   theme(text = element_text(size = 20), axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))
 ggsave(filename = file.path(TARGET_dir, "violin_btl.png"), plot = p, width = 10, height = 6)
 
@@ -132,7 +132,7 @@ p = ggplot(UMAP_coord, aes(x=reorder(cell_type, -Mipp1), y=Mipp1, fill = cell_ty
   theme_minimal() +
   scale_fill_brewer(palette = 'Set2', breaks=c('Early Tracheal Cells', 'Interm. Tracheal Cells', 'Late Tracheal Cells', 'Tracheal Tip Cells')) + 
   ylab("Mipp1 Normalized Expression") + 
-  xlab("cell type") + 
+  xlab("Cell Types") + 
   theme(text = element_text(size = 20), axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))
 ggsave(filename = file.path(TARGET_dir, "violin_Mipp1.png"), plot = p, width = 10, height = 6)
 
